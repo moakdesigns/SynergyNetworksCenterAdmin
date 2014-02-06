de9acb68ad08d2afc0336397a0ba970fe3e8d3a3
moakdesigns <moakdesigns@gmail.com>
2014-02-06 13:09:03 -0500

    log - upload error

---
+++ fuel/app/logs/2014/02/06.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-02-06 16:54:32 --> Compile Error - Upload\Autoloader::autoload(): Failed opening required 'C:\xampp\htdocs\SynergyNetworksCenterAdmin\fuel\app\vendor\codeguy\upload\src\Upload/../finfo.php' (include_path='.;C:\xampp\php\PEAR') in C:\xampp\htdocs\SynergyNetworksCenterAdmin\fuel\app\vendor\codeguy\upload\src\Upload\Autoloader.php on line 75
